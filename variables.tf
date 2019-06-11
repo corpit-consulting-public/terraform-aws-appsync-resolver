0451c9c9b0038543753240b90dd1ea33ae82e29a
Circular-test <nmartinez@corpit-consulting.com>
2019-06-11 17:51:29 -0300

    New README file

--- variables.tf
+++ variables.tf
@@ -3,35 +3,29 @@
 variable "api_id" {
   type        = "string"
   description = "The API ID for the GraphQL APi"
-  default     = ""
 }
 
 variable "type" {
   type        = "string"
   description = "The type name from the schema defined in the GraphQL API"
-  default     = ""
 }
 
 variable "field" {
   type        = "string"
   description = "The field name from the schema defined in the GraphQL API"
-  default     = ""
 }
 
 variable "data_source" {
   type        = "string"
   description = "The DataSource name"
-  default     = ""
 }
 
 variable "request_template" {
   type        = "string"
   description = "The request mapping template for this resolver"
-  default     = ""
 }
 
 variable "response_template" {
   type        = "string"
   description = "The response mapping template for this resolver"
-  default     = ""
 }
